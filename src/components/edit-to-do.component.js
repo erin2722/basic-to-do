859cdbecce6545cbbb8596c897fdeb2d566fb27b
Erin McNulty <erin2722@gmail.com>
2019-12-24 11:12:42 -0500

    completed part 1

---
+++ src/components/edit-to-do.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EditToDoList extends Component {
+    render() {
+        return (
+            <div>
+                <p>the edit todos component</p>
+            </div>
+        )
+    }
+}
+
+export default EditToDoList;
---
+++ src/components/todos-list.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ToDoList extends Component {
+    render() {
+        return (
+            <div>
+                <p>the todos component</p>
+            </div>
+        )
+    }
+}
+
+export default ToDoList;
